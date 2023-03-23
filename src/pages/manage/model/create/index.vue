232d0c3a087ad07a16950c7a9be67e56cf77d5bc
wanggang <wanggang@shwpg.com>
2023-03-23 18:21:31 +0800

    add new property function int/float/string/bool field

--- src/pages/manage/model/create/index.vue
+++ src/pages/manage/model/create/index.vue
@@ -9,6 +9,7 @@
 
     <t-card :bordered="false">    
       <p class="text-size-[18px] font-bold mt-2 mb-6">基本信息</p>
+
       <t-form ref="form" :data="basicFormData" :rules="BASIC_FORM_RULES">
         <t-form-item label="行业类型" name="category">
           <t-select v-model="basicFormData.category" :options="options1"></t-select>
@@ -35,7 +36,7 @@
           <p class="text-size-[18px] font-bold leading-loose">功能点</p>
         </t-col>
         <t-col :span="6" class="flex justify-end">
-          <t-button theme="primary">添加功能点</t-button>
+          <t-button theme="primary" @click="handleCreate">添加功能点</t-button>
         </t-col>
       </t-row>
 
@@ -53,13 +54,136 @@
       </t-table>
 
     </t-card>
+
+    <!-- 添加功能点 -->
+    <t-drawer v-model:visible="visible" header="添加功能点" size="medium" :on-confirm="handleConfirm">
+      <t-form ref="form" :rules="ITEM_FORM_RULES" :data="itemFormData" label-align="top" >
+        <t-form-item label="功能类型" name="action">
+          <t-select v-model="itemFormData.action" :options="actionOptions"></t-select>
+        </t-form-item>
+        <t-form-item label="功能名称" name="name">
+          <t-input v-model="itemFormData.name" placeholder="1-32位，中文、英文、数字及特殊字符_-，必须以中文或英文字符开头" />
+        </t-form-item>
+        <t-form-item label="标识符" name="code">
+          <t-input v-model="itemFormData.code" placeholder="1-32位，支持英文、数字及特殊字符_-，必须以英文字符开头" />
+        </t-form-item>
+        <t-form-item label="数据类型" name="type">
+          <t-select v-model="itemFormData.type" :options="typeOptions"></t-select>
+        </t-form-item>
+
+        <!-- int float 特有 -->
+        <t-form-item label="取值范围" name="range">
+          <t-range-input :inputProps="rangeInputProps"/>
+        </t-form-item>
+        <t-form-item label="数据精度">
+          <t-input v-model="itemFormData.precision" placeholder="请输入数据精度" />
+        </t-form-item>
+        <t-form-item label="数据单位">
+          <t-select v-model="itemFormData.uint" :options="uintOptions"></t-select>
+        </t-form-item>
+
+        <!-- string特有 -->
+        <t-form-item label="数据长度" name="length">
+          <t-input v-model="itemFormData.length" placeholder="请输入数据长度" /> <p class="min-w-[30px] ml-2">字符</p>
+        </t-form-item>
+
+        <!-- bool特有 -->
+        <t-form-item label="布尔值" name="binary">
+          <t-range-input :inputProps="binaryInputProps"/>
+        </t-form-item>
+
+
+        <t-form-item label="读写类型" name="permission">
+          <t-select v-model="itemFormData.permission" :options="permissionOptions"></t-select>
+        </t-form-item>
+        <t-form-item label="描述信息">
+          <t-textarea
+            v-model="basicFormData.description"
+            placeholder="请输入描述信息"
+            :autosize="{ minRows: 4 }"
+          />
+        </t-form-item>
+        
+      </t-form>
+    </t-drawer>
   </div>
 </template>
 
 <script setup>
-import { ref } from 'vue'
+import { ref, reactive } from 'vue'
 import { useRouter } from 'vue-router'
 import { ArrowLeftIcon } from 'tdesign-icons-vue-next'
+
+const visible = ref(false)
+
+const handleCreate = () => {
+  visible.value = true
+}
+
+const handleConfirm = () => {
+  visible.value = false
+}
+
+const ITEM_FORM_RULES = {
+  action: [{ required: true, message: '功能必须选择' }],
+  name: [{ required: true, message: '请输入功能名称' }],
+  code: [{ required: true, message: '请输入功能名称' }],
+  type: [{ required: true, message: '请选择数据类型' }],
+  length: [{ required: true, message: '请选择数据长度' }],
+  binary: [{ required: true, message: '请设置布尔含义' }],
+  range: [{ required: true, message: '请输入数据范围' }],
+  permission: [{ required: true, message: '请选择读写方式' }],
+}
+
+const itemFormData = reactive({
+  action: '1',
+  name: '',
+  code: '',
+  type: '1',
+  length: '',
+  min: '',
+  max: '',
+  precision: '',
+  truer: '',
+  falser: '',
+  uint: '',
+  permission: '1',
+})
+
+const actionOptions = [
+  { label: '属性类型', value: '1'},
+  { label: '事件类型', value: '2' },
+  { label: '服务类型', value: '3' },
+]
+
+const typeOptions = [
+  { label: 'int(整数型)', value: '1'},
+  { label: 'float(浮点型)', value: '2' },
+  { label: 'string(字符串)', value: '3' },
+  { label: 'bool(布尔)', value: '4' },
+  { label: 'enum(枚举)', value: '5' },
+  { label: 'struct(结构体)', value: '6' },
+  { label: 'array(数组)', value: '7' },
+]
+
+const rangeInputProps = [{ label: '最小值', name: 'min' }, { label: '最大值', name: 'max' }]
+const binaryInputProps = [{ label: 'true', name: 'min' }, { label: 'false', name: 'max' }]
+
+
+const uintOptions = [
+  { label: '平方厘米 / c㎡', value: '1'},
+  { label: '毫米 / mm', value: '2' },
+  { label: '千米 / km', value: '3' },
+  { label: '毫升 / mL', value: '4' },
+  { label: '帕斯卡 / Pa', value: '5' },
+  { label: '秒 / ″', value: '6' },
+  { label: '弧度 / rad', value: '7' },
+]
+
+const permissionOptions = [
+  { label: '读写', value: '1'},
+  { label: '只读', value: '2' },
+]
 
 const propertyColumns = [
   {colKey: 'name', title: '名称'},
